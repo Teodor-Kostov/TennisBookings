82b935f4de57efec93076883cc230a3a571869a7
Teodor-Kostov <teodorkostov88@gmail.com>
2026-04-16 23:21:47 +0200

    Add new types

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -43,4 +43,73 @@ export type Maybe<T> = T | null | undefined;
   export type ErrorResponse = {
     message: string;
   }
+  //For crud
+  export type BookingRaw = {
+    _id: string;
+    court: string;
+    user: string;
+    date: string;
+    startTime: string;
+    endTime: string;
+    deleted: boolean;
+    created_at: string;
+    updatedAt: string;
+  }
+
+  //Populated Booking
+  export type Booking = {
+    _id: string;
+    court: Court;
+    user: User;
+    date: string;
+    startTime: string;
+    endTime: string;
+    deleted: boolean;
+    created_at: string;
+    updatedAt: string;
+  }
+
+   export type CourtType = 'Clay' | 'Hard' ;
+
+  export type Court = {
+    _id: string;
+    number: number;
+    type: CourtType;
+    isActive: boolean;
+    created_at: string;
+    updatedAt: string;
+  }
+// InstertDto's
+  export type CreateCourt = {
+    number: number;
+    type: CourtType;
+    isActive: boolean;
+  }
+
+  export type CreateBooking = {
+    court: string;
+    user: string;
+    date: string;
+    startTime: string;
+    endTime: string;
+  }
+
+  export type BusySlot = {
+    court: {
+      number: number;
+      type: CourtType;
+    };
+    user: {
+      _id: string;
+      username: string;
+    };
+    date: string;
+    startTime: string;
+    endTime: string;
+  }
+
+  export type TimeSlot = {
+  start: string;
+  end: string;
+}
 
