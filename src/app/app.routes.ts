7a1a8524e65b314835c1ccaa41bca24df5c8a585
Teodor-Kostov <teodorkostov88@gmail.com>
2026-04-21 10:21:36 +0200

    add routes with guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,22 @@ import { RegisterComponent } from './auth/register/register.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { TennisSlotsComponent } from './tennis/tennis-slots/tennis-slots.component';
 import { MyBookingsComponent } from './booking/my-bookings/my-bookings.component';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
+import { CourtsComponent } from './tennis/courts/courts/courts.component';
+import { AddCourtComponent } from './tennis/courts/add-court/add-court.component';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
+  //for all users
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'slots', component: TennisSlotsComponent},
-  {path: 'my-bookings', component: MyBookingsComponent},
+  //for logged in users
+  {path: 'slots', component: TennisSlotsComponent, canActivate: [authGuard]},
+  {path: 'my-bookings', component: MyBookingsComponent, canActivate: [authGuard]},
+  {path: 'profile', component: UserProfileComponent, canActivate: [authGuard]},
+  {path: 'courts', component: CourtsComponent, canActivate: [authGuard]},
+  {path: 'courts/add', component: AddCourtComponent, canActivate: [authGuard]},
 ];
